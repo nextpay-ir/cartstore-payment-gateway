759ab248037b9c1b09c70c9d46eb93e30197da7a
Tayebe <st.hosseini92@gmail.com>
2018-10-27 14:18:31 +0330

    add sql file

---
+++ cs_module.sql
@@ -0,0 +1,3 @@
+INSERT INTO `cs_module` (`moduleName`, `moduleFileName`, `moduleType`, `moduleData`, `moduleStatus`) VALUES
+('نکست پی', 'nextpay', 0, 'a:1:{s:6:\"apiKey\";a:2:{s:5:\"label\";s:8:\"کد api\";s:5:\"value\";s:36:\"xxxxxxxx-0000-0000-0000-111111111111\";}}', 1);
+
